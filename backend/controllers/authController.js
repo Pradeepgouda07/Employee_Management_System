e5f76143d8ec3f9c44a2504cfd80c779f08380ee
Pradeepgouda07 <pradeepgowda9470Agmail.com>
2025-11-12 13:09:07 +0530

    Initial commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,28 +1,3 @@
-// const User = require('../models/User');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-// const { jwtSecret } = require('../config');
-
-// exports.register = async (req, res) => {
-//     const { name, email, password, role } = req.body;
-//     const hashed = await bcrypt.hash(password, 10);
-//     const user = new User({ name, email, password: hashed, role });
-//     await user.save();
-//     res.status(201).send('User Registered');
-// };
-
-// exports.login = async (req, res) => {
-//     const { email, password } = req.body;
-//     const user = await User.findOne({ email });
-//     if(!user) return res.status(400).send('Invalid Credentials');
-
-//     const match = await bcrypt.compare(password, user.password);
-//     if(!match) return res.status(400).send('Invalid Credentials');
-
-//     const token = jwt.sign({ id: user._id, role: user.role }, jwtSecret, { expiresIn: '1d' });
-//     res.json({ token, role: user.role });
-// };
-
 
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
